cfafdad62cdbc5d3386efbe4d75d6061126c10e1
Muskovszky Tamás <muskovszky.tamas@burg.lo.hu>
2024-09-09 14:31:17 +0200

    Util function factorial

--- functions.js
+++ functions.js
@@ -2,8 +2,18 @@ function getFirstElement(arr){
     return arr[0];
 }
 
+function factorial(num){
+    let factorial = 1
+    for (let i = 2; i <= num; i++){
+        factorial *= i;
+    }
+    return factorial
+}
+
 const numbers = [1, 2,3]
 const words = ["apple", "banana", "cherry"]
 
 console.log(`first number: ${getFirstElement(numbers)}`)
 console.log(`first word: ${getFirstElement(words)}`)
+
+console.log(factorial(5))
