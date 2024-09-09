f0c114b3b21271a0d442ee3aec295ed8cc3277c1
Muskovszky Tamás <muskovszky.tamas@burg.lo.hu>
2024-09-09 14:25:30 +0200

    first add

---
+++ functions.js
@@ -0,0 +1,9 @@
+function getFirstElement(arr){
+    return arr[0];
+}
+
+const numbers = [1, 2,3]
+const words = ["apple", "banana", "cherry"]
+
+console.log(`first number: ${getFirstElement(numbers)}`)
+console.log(`first word: ${getFirstElement(words)}`)
