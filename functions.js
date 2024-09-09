1af31736ea557adebf123920b7b9a9c8d57b3394
Muskovszky Tamás <muskovszky.tamas@burg.lo.hu>
2024-09-09 14:33:20 +0200

    all element

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
 function getFirstElement(arr){
-    return arr[0];
+    arr.forEach(element => {
+        console.log(element)
+    });;
 }
 
 const numbers = [1, 2,3]
 const words = ["apple", "banana", "cherry"]
 
-console.log(`first number: ${getFirstElement(numbers)}`)
-console.log(`first word: ${getFirstElement(words)}`)
